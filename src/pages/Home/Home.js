abee95d50c7a0bb8b8235e7ed4096aa1164ec3b6
Luchtml <luchtml5@gmail.com>
2023-03-06 12:08:00 -0300

    pre files e style

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import api from '../../services/api';
+import { MdFlightTakeoff } from 'react-icons/md';
+import './style.css'
 
 const Home = () => {
-  return <div>Home</div>;
+  const [trips, setTrips] = React.useState([]);
+
+  React.useEffect(() => {
+    async function loadApi() {
+      const response = await api.get('trips');
+      setTrips(response.data);
+    }
+
+    loadApi();
+  }, []);
+
+  return (
+    <div>
+      <div className="box">
+        {trips.map((trip) => (
+          <li key={trip.id}>
+            <img src={trip.image} alt={trip.title} />
+            <strong> {trip.title}</strong>
+            <span>Status: {trip.status ? 'Disponivel' : 'Indisponivel'}</span>
+            <button type="button" onClick={() => {}}>
+              <div>
+                <MdFlightTakeoff size={16} color="#FFF" />
+              </div>
+              <span>SOLICITAR RESERVA</span>
+            </button>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
