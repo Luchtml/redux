7fb1dd7a8d14315dd8fdd8a2e84b2fd48dcd6544
Luchtml <luchtml5@gmail.com>
2023-03-06 11:42:02 -0300

    replaces

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import './style.css'
+import './style.css';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 
 const Header = () => {
   return (
     <header className="container">
-      <Link to='/'>
-        <img className="logo" src={logo} alt='Logo do projeto'/>
+      <Link to="/">
+        <img className="logo" src={logo} alt="Logo do projeto" />
       </Link>
-      <Link className='reserva' to='/reservas'>
-        <strong>Minhas Reservas</strong>
-        <span>3 Reservas</span>
+      <Link className="reserva" to="/reservas">
+        <div>
+          <strong>Minhas Reservas</strong>
+          <span>3 Reservas</span>
+        </div>
       </Link>
     </header>
   );
