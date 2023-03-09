9cc093728ff09f7de56052be77c7db8c0998e916
Luchtml <luchtml5@gmail.com>
2023-03-09 17:49:45 -0300

    refatorando

--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,11 +1,26 @@
-import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { all, call, put, select, takeLatest } from 'redux-saga/effects';
 import api from '../../../services/api';
-import { addReserveSuccess } from './actions';
+import { addReserveSuccess, updateAmountReserve } from './actions';
 
 function* addToReserve({ id }) {
-  const response = yield call(api.get, `trips/${id}`);
+  const tripExists = yield select((state) =>
+    state.reserve.find((trip) => trip.id === id),
+  );
 
-  yield put(addReserveSuccess(response.data));
+  if (tripExists) {
+    const amount = tripExists.amount + 1;
+
+    yield put(updateAmountReserve(id, amount));
+  } else {
+    const response = yield call(api.get, `trips/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+    };
+
+    yield put(addReserveSuccess(data));
+  }
 }
 
 export default all([takeLatest('ADD_RESERVE_REQUEST', addToReserve)]);
